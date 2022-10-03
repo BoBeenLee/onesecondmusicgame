498b78e8dbc5ae9caa73672729fdafe357fe3eed
bobeenlee <globaldev@naver.com>
2022-10-03 20:55:46 +0900

    feat: 스토리북 설정 추가

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'transform-inline-environment-variables',
+      {
+        include: ['REACT_ENV'],
+      },
+    ],
+    ['@babel/plugin-proposal-decorators', {legacy: true}],
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['.'],
+      },
+    ],
+  ],
 };
